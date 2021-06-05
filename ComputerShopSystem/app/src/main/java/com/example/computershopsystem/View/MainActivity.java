976cdb42fedbc2d403a6007fc80975d2bbc5c97f
Khuong Nguyen <khuongkg1412@gmail.com>
2021-06-05 21:26:21 +0700

    run test branch Khuong

--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/MainActivity.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.computershopsystem.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.computershopsystem.R;
 
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  void  changePage(View view){
+        Intent intent = new Intent(this, ProductDetail.class);
+        startActivity(intent);
     }
 }
