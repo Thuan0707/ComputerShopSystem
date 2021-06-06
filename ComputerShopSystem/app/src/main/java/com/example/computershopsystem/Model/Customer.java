161e91cca85125d5066c02bbf7124933c1c8afe4
Thuan0707 <thuandmce140131@fpt.edu.vn>
2021-06-06 17:49:57 +0700

    a
    a

---
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/Model/Customer.java
@@ -0,0 +1,19 @@
+package com.example.computershopsystem.Model;
+
+import java.util.Date;
+
+public class Customer {
+    int id;
+    int cartID;
+    String phone;
+    String email;
+    String password;
+    String fullName;
+    Date dateOfBirth;
+    String address;
+    int gender;
+    long money;
+    Date createAt;
+    Date deleteAt;
+
+}
--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/MainActivity.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.computershopsystem.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
