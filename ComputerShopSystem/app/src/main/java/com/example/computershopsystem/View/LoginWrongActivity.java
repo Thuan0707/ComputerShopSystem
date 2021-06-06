c9e479133ebfdbcbc750b0bd1b442f8fc715a889
Nguyễn Trung Hiển <71125247+Hiennt4@users.noreply.github.com>
2021-06-06 22:41:34 +0700

    done 4
    aaaaa

---
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/LoginWrongActivity.java
@@ -0,0 +1,61 @@
+package com.example.computershopsystem.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.computershopsystem.R;
+
+import java.util.regex.Pattern;
+
+import static com.example.computershopsystem.R.id.btn_Login;
+
+public class LoginWrongActivity extends AppCompatActivity {
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    //"(?=.*[0-9])" +         //at least 1 digit
+                    //"(?=.*[a-z])" +         //at least 1 lower case letter
+                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                    "(?=.*[a-zA-Z])" +      //any letter
+                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                    "(?=\\S+$)" +           //no white spaces
+                    ".{4,}" +               //at least 4 characters
+                    "$");
+
+    private EditText textPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_wrong);
+
+        textPassword = findViewById(R.id.etPassword);
+    }
+    private boolean validatePassword() {
+        String passwordInput = textPassword.getText().toString().trim();
+        if (passwordInput.isEmpty()) {
+            textPassword.setError("Field can't be empty");
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+            textPassword.setError("Password too weak");
+            return false;
+        } else {
+            textPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void confirmInput(View v) {
+        if (!validatePassword()) {
+            return;
+        }
+        String input = "Password: " + textPassword.getText().toString();
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+    }
+}
--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/MainActivity.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/MainActivity.java
@@ -2,15 +2,76 @@ package com.example.computershopsystem.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.computershopsystem.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin, btnActive, btnWrong, btnRegister, btnHome, btnHomeLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        map();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnActive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActiveActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnWrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginWrongActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnHomeLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+
+    private void map() {
+        btnLogin = findViewById(R.id.btnLogin);
+        btnActive = findViewById(R.id.btnActive);
+        btnWrong = findViewById(R.id.btnWrong);
+        btnRegister = findViewById(R.id.btnREgis);
+        btnHome = findViewById(R.id.btnHome);
+        btnHomeLogin = findViewById(R.id.btnHomeLogin);
     }
 }
