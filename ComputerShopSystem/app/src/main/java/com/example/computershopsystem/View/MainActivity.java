a2598981fa8345e6c7b6695002845d10c6c42018
Thuan0707 <thuandmce140131@fpt.edu.vn>
2021-06-07 10:34:08 +0700

    a
    a

--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/MainActivity.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        DatabaseReference mDatabase=FirebaseDatabase.getInstance().getReference("mesagae");
-       mDatabase.setValue("Group 81222");
+       mDatabase.setValue("Group 81222222222222");
        Log.e("sadffffffffffffffffff",mDatabase.toString());
 
     }
