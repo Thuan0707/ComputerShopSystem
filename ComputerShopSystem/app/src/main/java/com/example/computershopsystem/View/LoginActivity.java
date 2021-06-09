aef31fdd865a3804731230d8cb842bfa1fe8bfac
Thuan0707 <thuandmce140131@fpt.edu.vn>
2021-06-09 18:13:33 +0700

    a
    a

--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/LoginActivity.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/LoginActivity.java
@@ -1,35 +1,40 @@
 package com.example.computershopsystem.View;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
 
 import com.example.computershopsystem.R;
 import com.example.computershopsystem.databinding.ActivityLoginBinding;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
-import com.google.firebase.auth.PhoneAuthProvider;
 
-import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
-
+    Button btnLo;
+    EditText etPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_login);
+        btnLo = findViewById(R.id.btnLo);
+        etPhone = findViewById(R.id.etPhone);
 
+        btnLo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this, LoginVerifyOTP.class);
+                intent.putExtra("PhoneNo",etPhone.getText().toString().trim());
+                startActivity(intent);
+            }
+        });
     }
 }
--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/LoginVerifyOTP.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/LoginVerifyOTP.java
@@ -3,19 +3,29 @@ package com.example.computershopsystem.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+
+import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 import com.example.computershopsystem.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,60 +35,110 @@ public class LoginVerifyOTP extends AppCompatActivity {
     private String mVerificationId;
     private FirebaseAuth firebaseAuth;
 
-
     Button btnVerify;
+    Button btnResend;
     EditText etOTP;
+    String phone = "";
     private static final String TAG = "LOGIN_TAG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_verify_otp);
+        phone = "";
+        phone = getIntent().getStringExtra("PhoneNo").trim();
+
+
         firebaseAuth = FirebaseAuth.getInstance();
         mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(@NonNull @org.jetbrains.annotations.NotNull PhoneAuthCredential phoneAuthCredential) {
-
+signInWithPhoneAuthCredentail(phoneAuthCredential);
             }
 
             @Override
             public void onVerificationFailed(@NonNull @org.jetbrains.annotations.NotNull FirebaseException e) {
-
+                Toast.makeText(LoginVerifyOTP.this,e.getMessage(),Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onCodeSent(@NonNull @org.jetbrains.annotations.NotNull String s, @NonNull @org.jetbrains.annotations.NotNull PhoneAuthProvider.ForceResendingToken token) {
-                super.onCodeSent(s, token);
-                mVerificationId=s;
-                forceResendingToken=token;
-            }
+            public void onCodeSent(@NonNull @org.jetbrains.annotations.NotNull String verificationId, @NonNull @org.jetbrains.annotations.NotNull PhoneAuthProvider.ForceResendingToken token) {
+                super.onCodeSent(verificationId, token);
 
+                Log.d(TAG,"onCodeSent"+verificationId);
+                mVerificationId = verificationId;
+                forceResendingToken = token;
+                Toast.makeText(LoginVerifyOTP.this,"Verification code sent",Toast.LENGTH_SHORT).show();
+
+            }
         };
+
+        startPhoneNumberVerification(phone);
+        btnResend = findViewById(R.id.btnResend);
         btnVerify = findViewById(R.id.btnVerify);
-        etOTP = findViewById(R.id.etPhone);
+        etOTP = findViewById(R.id.etOTP);
+        btnResend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resendVerificationCode(phone, forceResendingToken);
+            }
+        });
         btnVerify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String phone ="";
-                if (TextUtils.isEmpty(phone)) {
-                    Toast.makeText(LoginVerifyOTP.this, "Please enter phone number...", Toast.LENGTH_SHORT).show();
+                String code = btnVerify.getText().toString().trim();
+                if (TextUtils.isEmpty(code)) {
+                    Toast.makeText(LoginVerifyOTP.this, "Please enter code", Toast.LENGTH_SHORT).show();
                 } else {
-                    startPhoneNumberVerification("+84"+phone);
+                    verifyPhoneNumberWithCode(mVerificationId, code);
                 }
             }
         });
 
     }
 
+    private void verifyPhoneNumberWithCode(String verificationId, String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithPhoneAuthCredentail(credential);
+    }
+
+    private void resendVerificationCode(String phone, PhoneAuthProvider.ForceResendingToken token) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(firebaseAuth)
+                        .setPhoneNumber("+84" + phone)
+                        .setTimeout(60L, TimeUnit.SECONDS)
+                        .setActivity(this)
+                        .setCallbacks(mCallbacks)
+                        .setForceResendingToken(token)
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
 
     private void startPhoneNumberVerification(String phone) {
-        PhoneAuthOptions options=
+        PhoneAuthOptions options =
                 PhoneAuthOptions.newBuilder(firebaseAuth)
-                        .setPhoneNumber(phone)
+                        .setPhoneNumber("+84" + phone)
                         .setTimeout(60L, TimeUnit.SECONDS)
                         .setActivity(this)
                         .setCallbacks(mCallbacks)
                         .build();
         PhoneAuthProvider.verifyPhoneNumber(options);
+    }
 
+    private void signInWithPhoneAuthCredentail(PhoneAuthCredential credential) {
+        firebaseAuth.signInWithCredential(credential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                String phone = firebaseAuth.getCurrentUser().getPhoneNumber();
+                Toast.makeText(LoginVerifyOTP.this, "Logged in as" + phone, Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull @NotNull Exception e) {
+                Toast.makeText(LoginVerifyOTP.this, e.getMessage() + phone, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
