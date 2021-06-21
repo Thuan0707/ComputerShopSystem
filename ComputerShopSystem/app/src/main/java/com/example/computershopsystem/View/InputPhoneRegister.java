16b3727fd4f489251f2be2b781eb8307de952708
Thuan0707 <thuandmce140131@fpt.edu.vn>
2021-06-21 07:55:41 +0700

    a
    a

--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/InputPhoneRegister.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/InputPhoneRegister.java
@@ -3,7 +3,7 @@ package com.example.computershopsystem.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import com.example.computershopsystem.R;
 public class InputPhoneRegister extends AppCompatActivity {
 
     @Override
