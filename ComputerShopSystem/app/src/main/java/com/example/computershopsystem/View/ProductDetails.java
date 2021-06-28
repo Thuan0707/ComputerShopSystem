637a0061fe94db9e0bf48d40579f141d18a582a6
Thuan0707 <thuandmce140131@fpt.edu.vn>
2021-06-28 07:36:04 +0700

    a
    a

--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/ProductDetails.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/ProductDetails.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.computershopsystem.Model.Product;
 import com.example.computershopsystem.R;
 
 public class ProductDetails extends AppCompatActivity {
 
+    public Product product;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
     }
 }
