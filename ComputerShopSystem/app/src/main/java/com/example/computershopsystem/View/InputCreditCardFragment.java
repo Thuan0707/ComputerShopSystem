81e4fe431f9d851cd74c8604ea8a217e82a44303
Phan Thanh Liem <43318029+liemphan@users.noreply.github.com>
2021-07-05 17:22:31 +0700

    update Credit Card

--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/InputCreditCardFragment.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/InputCreditCardFragment.java
@@ -177,8 +177,14 @@ public class InputCreditCardFragment extends Fragment {
                     CreditCard creditCard = new CreditCard(cardNumber, cardMoney, cardNumber, cardExpiration, cardHolder, new Date(), new Date());
                     databaseReference = FirebaseDatabase.getInstance().getReference().child("Customer").child(firebaseUser.getUid()).child("card");
                     databaseReference.push().setValue(creditCard);
-
-                    // chuyen trang o day
+                    CreditCardFragment fragment = new CreditCardFragment();
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragTransaction = fragmentManager.beginTransaction();
+                    fragTransaction.setCustomAnimations(android.R.animator.fade_in,
+                            android.R.animator.fade_out);
+                    fragTransaction.addToBackStack(null);
+                    fragTransaction.replace(R.id.fl_wrapper, fragment);
+                    fragTransaction.commit();
                 }
             }
         });
