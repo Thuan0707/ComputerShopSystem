5ea2c830eaf7bc6a35d535a736687a8815346089
Phan Thanh Liem <43318029+liemphan@users.noreply.github.com>
2021-07-05 11:32:43 +0700

    updateCreditCard

--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/CreditCardFragment.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/CreditCardFragment.java
@@ -7,24 +7,53 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.example.computershopsystem.Addapter.LVCreditCardAdapter;
+import com.example.computershopsystem.Addapter.LVProductInCartAdapter;
+import com.example.computershopsystem.Model.CartProduct;
+import com.example.computershopsystem.Model.CreditCard;
+import com.example.computershopsystem.Model.Product;
 import com.example.computershopsystem.R;
 
+import com.example.computershopsystem.databinding.CartFragmentBinding;
 import com.example.computershopsystem.databinding.CreditCardFragmentBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CreditCardFragment extends Fragment {
     CreditCardFragmentBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private ListView listView;
+    SharedPreferences sharedpreferences;
+    SharedPreferences.Editor editor;
     @Nullable
     @Override
     public View onCreateView(@NonNull  LayoutInflater inflater, @Nullable  ViewGroup container, @Nullable  Bundle savedInstanceState) {
         binding = CreditCardFragmentBinding.inflate(getLayoutInflater());
         View view=binding.getRoot();
+        listView = binding.lvCard;
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        sharedpreferences = getActivity().getSharedPreferences(firebaseUser.getUid(), Context.MODE_PRIVATE);
+        editor = sharedpreferences.edit();
+        List<CreditCard> productList = getList();
+        LVCreditCardAdapter LVCreditCardAdapter = new LVCreditCardAdapter(getActivity(), R.layout.card_item, productList);
+        listView.setAdapter(LVCreditCardAdapter);
         binding.btnGotoAddCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,5 +68,17 @@ public class CreditCardFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    public List<CreditCard> getList() {
+        List<CreditCard> listProduct = new ArrayList<>();
+        String serializedObject = sharedpreferences.getString("cart", null);
+        if (serializedObject != null) {
+            Gson gson = new Gson();
+            Type type = new TypeToken<List<CreditCard>>() {
+            }.getType();
+            listProduct = gson.fromJson(serializedObject, type);
+        }
+        return listProduct;
     }
 }
--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/InputCreditCardFragment.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/InputCreditCardFragment.java
@@ -5,24 +5,62 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.computershopsystem.Model.CreditCard;
 import com.example.computershopsystem.R;
 import com.example.computershopsystem.databinding.CreditCardFragmentBinding;
 import com.example.computershopsystem.databinding.InputCreditCardFragmentBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class InputCreditCardFragment extends Fragment {
-InputCreditCardFragmentBinding binding;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    DatabaseReference databaseReference;
+    InputCreditCardFragmentBinding binding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull  LayoutInflater inflater, @Nullable  ViewGroup container, @Nullable  Bundle savedInstanceState) {
         binding = InputCreditCardFragmentBinding.inflate(getLayoutInflater());
         View view=binding.getRoot();
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        binding.btnAddCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cardNumber = String.valueOf(binding.edCardNumberAdd.getText());
+                Date cardExpiration = new Date();
+                try {
+                    cardExpiration = formatter.parse(String.valueOf(binding.edExpirationCardAdd.getText())) ;
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                String cardHolder = String.valueOf(binding.edCardHolderAdd.getText());
+                String cardMoney = String.valueOf(binding.edMoneyCardAdd.getText());
+                CreditCard creditCard = new CreditCard(cardNumber,cardMoney,cardNumber,null,cardHolder,null,null);
+                databaseReference = FirebaseDatabase.getInstance().getReference().child("Customer").child(firebaseUser.getUid());
+                databaseReference.push().setValue(creditCard);
+            }
+        });
 
         return  view;
     }
