70d7822ef4837df91ff8caaab37e6fe1bf130cee
Thuan0707 <thuandmce140131@fpt.edu.vn>
2021-07-13 19:19:29 +0700

    a
    a

--- ComputerShopSystemAdmin/app/src/main/java/com/example/computershopsystemadmin/MainActivity.java
+++ ComputerShopSystemAdmin/app/src/main/java/com/example/computershopsystemadmin/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+
+public class MainActivity extends AppCompatActivity {
+DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                mDatabase = FirebaseDatabase.getInstance().getReference("abc");
+                String  id=mDatabase.push().getKey();
+
+        mDatabase.child(id).setValue("ádfsaf");
     }
 }
