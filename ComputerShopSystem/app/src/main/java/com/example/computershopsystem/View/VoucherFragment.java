c7a5a71b6ac57744d597ed5afeb78d06e550abf4
Thuan0707 <thuandmce140131@fpt.edu.vn>
2021-07-20 13:01:06 +0700

    a
    a

--- ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/VoucherFragment.java
+++ ComputerShopSystem/app/src/main/java/com/example/computershopsystem/View/VoucherFragment.java
@@ -22,6 +22,7 @@ import com.example.computershopsystem.databinding.VoucherFragmentBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
 
 public class VoucherFragment extends Fragment {
     VoucherFragmentBinding binding;
@@ -31,36 +32,34 @@ public class VoucherFragment extends Fragment {
 CreditCard creditCard;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = VoucherFragmentBinding.inflate(getLayoutInflater());
-        sharedpreferences = getActivity().getSharedPreferences(FirebaseAuth.getInstance().getCurrentUser().getUid(), Context.MODE_PRIVATE);
-        editor = sharedpreferences.edit();
-        databaseReference = FirebaseDatabase.getInstance().getReference("Voucher");
-        VoucherFirebaseHelper helper = new VoucherFirebaseHelper(databaseReference, getContext());
-        helper.getList(binding.lvVoucher);
-        Bundle bundle=this.getArguments();
-        binding.lvVoucher.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Voucher voucher = (Voucher) parent.getAdapter().getItem(position);
-           // creditCard=(CreditCard)bundle.getSerializable("creditCard");
-
-                bundle.putSerializable("voucher", voucher);
-                CheckOutFragment fragment = new CheckOutFragment();
-                fragment.setArguments(bundle);
-                switchFragment(fragment);
-            }
-        });
-        View view = binding.getRoot();
-        return view;
+      binding = VoucherFragmentBinding.inflate(getLayoutInflater());
+      sharedpreferences = getActivity().getSharedPreferences(FirebaseAuth.getInstance().getCurrentUser().getUid(), Context.MODE_PRIVATE);
+      editor = sharedpreferences.edit();
+      databaseReference = FirebaseDatabase.getInstance().getReference("Voucher");
+      VoucherFirebaseHelper helper=new VoucherFirebaseHelper(databaseReference,getContext());
+      helper.getList(binding.lvVoucher);
+      binding.lvVoucher.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+          Voucher voucher=(Voucher)parent.getAdapter().getItem(position);
+          Gson gson = new Gson();
+          String json = gson.toJson(voucher);
+          editor.putString("voucher", json);
+          editor.apply();
+          CheckOutFragment fragment = new CheckOutFragment();
+          switchFragment(fragment);
+        }
+      });
+      View view=binding.getRoot();
+      return  view;
     }
-
-    void switchFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        FragmentTransaction fragTransaction = fragmentManager.beginTransaction();
-        fragTransaction.setCustomAnimations(android.R.animator.fade_in,
-                android.R.animator.fade_out);
-        fragTransaction.addToBackStack(null);
-        fragTransaction.replace(R.id.fl_wrapper, fragment);
-        fragTransaction.commit();
-    }
+  void switchFragment(Fragment fragment) {
+    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+    FragmentTransaction fragTransaction = fragmentManager.beginTransaction();
+    fragTransaction.setCustomAnimations(android.R.animator.fade_in,
+            android.R.animator.fade_out);
+    fragTransaction.addToBackStack(null);
+    fragTransaction.replace(R.id.fl_wrapper, fragment);
+    fragTransaction.commit();
+  }
 }
